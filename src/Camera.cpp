2b90d224c1f54bc3357d0f8840c529181fd0ee3c
Olivier Deiss <olivier.deiss@gmail.com>
2016-03-15 22:47:11 +0100

    documentation

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -10,11 +10,16 @@ License: This software is offered under the GPL license. See COPYING for more in
     #include <GLUT/glut.h>
 #endif
 
-#include <iostream>
-
-#include "Camera.hpp"
-
-Camera::Camera(float p_X, float p_Y, float p_Z, float p_psi, float p_theta, float p_rotation_speed, float p_translation_speed, int p_window_width, int p_window_height) :
+/*
+Initializes the variables and creates the key map according to the
+given keyboard configuration. On azerty keyboards, 'z', 'q', 's', 'd'
+and on qwerty keyboards, 'w', 'a', 's', 'd', allow the user to move
+in the 3D world with the left hand while controling the mouse with
+the right hand. Indeed, these letters are on the left-hand side of
+the keyboard and offer a configuration similar to the arrow pad's one.
+*/
+Camera::Camera(KEYBOARD p_keyboard, float p_X, float p_Y, float p_Z, float p_psi, float p_theta, float p_rotation_speed, float p_translation_speed, int p_window_width, int p_window_height) :
+    keyboard(p_keyboard),
     X(p_X),
     Y(p_Y),
     Z(p_Z),
@@ -25,9 +30,29 @@ Camera::Camera(float p_X, float p_Y, float p_Z, float p_psi, float p_theta, floa
     rotation_speed(p_rotation_speed),
     translation_speed(p_translation_speed),
     time(0) {
-    for(int i=0 ; i<255 ; i++) { keyboard[i] = false; }
+    for(int i=0 ; i<255 ; i++) { keys[i] = false; }
+    switch(keyboard) {
+        case AZERTY:
+            key_map[FORWARD]  = static_cast<int>('z');
+            key_map[BACKWARD] = static_cast<int>('s');
+            key_map[LEFT]     = static_cast<int>('q');
+            key_map[RIGHT]    = static_cast<int>('d');
+            break;
+        case QWERTY:
+            key_map[FORWARD]  = static_cast<int>('w');
+            key_map[BACKWARD] = static_cast<int>('s');
+            key_map[LEFT]     = static_cast<int>('q');
+            key_map[RIGHT]    = static_cast<int>('d');
+            break;
+    }
 }
 
+/*
+Computes the new angle values given the mouse direction. The sight's
+only limits are when looking up or down: it is impossible for the user to
+look at his feets or straight up in the air (these edge cases would need
+a special treatment in the gluLookAt function).
+*/
 void Camera::rotation(int x, int y) {
     theta -= static_cast<float>(x - mouse_x) * rotation_speed;
     psi   += static_cast<float>(y - mouse_y) * rotation_speed;
@@ -37,26 +62,31 @@ void Camera::rotation(int x, int y) {
     mouse_y = y;
 }
 
+/*
+Computes the new sphere center given the speed and direction of the camera.
+The direction depends on the current angles values, the keys being pushed,
+and the elapsed time since the last function call.
+*/
 void Camera::translation() {
     float t = static_cast<float>(glutGet(GLUT_ELAPSED_TIME) - time);
     time    = glutGet(GLUT_ELAPSED_TIME);
-    if(keyboard[100]) { // d
-        X -= sin(theta + M_PI/2)*sin(psi) * translation_speed * t;
-        Z -= cos(
-        theta + M_PI/2)*sin(psi) * translation_speed * t;
+    if(keys[key_map[FORWARD]]) {
+        X += sin(theta)*sin(psi) * translation_speed * t;
+        Y += cos(psi)            * translation_speed * t;
+        Z += cos(theta)*sin(psi) * translation_speed * t;
     }
-    if(keyboard[113]) { // q
-        X -= sin(theta - M_PI/2)*sin(psi) * translation_speed * t;
-        Z -= cos(theta - M_PI/2)*sin(psi) * translation_speed * t;
-    }
-    if(keyboard[115]) { // s
+    if(keys[key_map[BACKWARD]]) {
         X -= sin(theta)*sin(psi) * translation_speed * t;
         Y -= cos(psi)            * translation_speed * t;
         Z -= cos(theta)*sin(psi) * translation_speed * t;
     }
-    if(keyboard[122]) { // z
-        X += sin(theta)*sin(psi) * translation_speed * t;
-        Y += cos(psi)            * translation_speed * t;
-        Z += cos(theta)*sin(psi) * translation_speed * t;
+    if(keys[key_map[LEFT]]) {
+        X -= sin(theta - M_PI/2)*sin(psi) * translation_speed * t;
+        Z -= cos(theta - M_PI/2)*sin(psi) * translation_speed * t;
+    }
+    if(keys[key_map[RIGHT]]) {
+        X -= sin(theta + M_PI/2)*sin(psi) * translation_speed * t;
+        Z -= cos(
+        theta + M_PI/2)*sin(psi) * translation_speed * t;
     }
 }
--- src/Camera.hpp
+++ src/Camera.hpp
@@ -4,16 +4,29 @@ Author:  DEISS Olivier
 License: This software is offered under the GPL license. See COPYING for more information.
 */
 
+/*
+This class defines a camera for a 3D scene, to be used with the glut library.
+The camera is implemented as a sphere. Its center defines the position of the camera,
+and one point on the sphere surface defines the sight. The calculus involves psi and
+theta angles, which represent the spherical coordinates system.
+To look around, call rotation() from the mouse event function, and to move, call
+translation() from the keyboard event function. Then, using the get*() functions,
+call the gluLookAt() function with the matching parameters.
+*/
+
 #ifndef CAMERAHPP
 #define CAMERAHPP
 
 #include <cmath>
+#include <map>
 
 class Camera {
 
     public :
     
-        Camera(float, float, float, float, float, float, float, int, int);
+        enum KEYBOARD {AZERTY, QWERTY};   /* type of keyboard */
+    
+        Camera(KEYBOARD, float, float, float, float, float, float, float, int, int);
         ~Camera() {}
     
         int   getMouseX() { return mouse_x; }
@@ -25,7 +38,7 @@ class Camera {
         float getY()      { return Y; }
         float getZ()      { return Z; }
 
-        void setKeyboard(int i, bool state) { keyboard[i] = state; }
+        void setKeyboard(int i, bool state) { keys[i] = state; }
         void setMouse(int x, int y)         { mouse_x = x; mouse_y = y; }
         void setX(float p_X)                { X = p_X; }
         void setY(float p_Y)                { Y = p_Y; }
@@ -36,18 +49,22 @@ class Camera {
     
     private :
     
-        bool  keyboard[255];      // to know if one specific key is up or down. UP = false
-        
-        float X;
-        float Y;
-        float Z;
-        int   mouse_x;
-        int   mouse_y;
-        float psi;
-        float theta;
-        float rotation_speed;
-        float translation_speed;
-        int   time;
+        enum    DIR {FORWARD, BACKWARD, LEFT, RIGHT};   /* translation direction */
+        typedef std::map<DIR, int> k_map;
+    
+        k_map    key_map;                               /* associations keys - directions */
+        KEYBOARD keyboard;                              /* keyboard type */
+        bool     keys[255];                             /* keys being pushed (true if pushed) */
+        float    X;                                     /* sphere center x axis */
+        float    Y;                                     /* sphere center y axis */
+        float    Z;                                     /* sphere center z axis */
+        int      mouse_x;                               /* mouse x position */
+        int      mouse_y;                               /* mouse y position */
+        float    psi;                                   /* psi angle in spherical coordinates */
+        float    theta;                                 /* theta angle in spherical coordinates */
+        float    rotation_speed;                        /* equivalent to mouse sensibility */
+        float    translation_speed;                     /* equivalent to keyboard sensibility */
+        int      time;                                  /* to keep track of the time between two function calls */
     
 };
                                                                                  
