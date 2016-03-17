34ff770d770d28fe333e529359567183d3c0e16c
Olivier Deiss <olivier.deiss@gmail.com>
2016-03-17 17:33:59 +0100

    license

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,7 +1,24 @@
 /*
-Project: Camera
-Author:  DEISS Olivier
-License: This software is offered under the GPL license. See COPYING for more information.
+Camera - Copyright (C) 2016 - Olivier Deiss - olivier.deiss@gmail.com
+
+Camera is a simple C++ 3D camera that you can use in any 3D
+environment. You can look around using the mouse, and move in
+all the directions using the keyboard. The calculus is made
+with simple trigonometry and the project is to be used with
+OpenGL/glut libraries.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifdef __linux__
